e2733d0b5065111ad655eed574389094567cf0de
fredgoum <alfredgoumou@gmail.com>
2021-01-02 17:50:28 +0100

    authentification by token and security

--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+const auth = require('../middlewares/auth');
+
 const stuffCtrl = require('../controllers/stuff');
 
 /* CRUD Methods */
-router.post('/', stuffCtrl.createThing);
-router.get('/', stuffCtrl.getAllThing);
-router.get('/:id', stuffCtrl.getOneThing);
-router.put('/:id', stuffCtrl.modifyThing);
-router.delete('/:id', stuffCtrl.deleteThing);
+router.post('/', auth, stuffCtrl.createThing);
+router.get('/', auth, stuffCtrl.getAllThing);
+router.get('/:id', auth, stuffCtrl.getOneThing);
+router.put('/:id', auth, stuffCtrl.modifyThing);
+router.delete('/:id', auth, stuffCtrl.deleteThing);
 
 module.exports = router;
